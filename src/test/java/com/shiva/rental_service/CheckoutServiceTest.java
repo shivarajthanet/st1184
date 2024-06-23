fc5b8a7ff9ced918e62a028a268ea5bdec920a7f
Shivaraj Thanet <shivarajthanet@gmail.com>
2024-06-23 23:11:01 +0530

    added negative test cases

--- src/test/java/com/shiva/rental_service/CheckoutServiceTest.java
+++ src/test/java/com/shiva/rental_service/CheckoutServiceTest.java
@@ -3,9 +3,13 @@ package com.shiva.rental_service;
 import com.shiva.rental_service.entities.Cart;
 import com.shiva.rental_service.entities.RentalAgreement;
 import com.shiva.rental_service.entities.Tool;
+import com.shiva.rental_service.exception.InvalidDateRangeException;
+import com.shiva.rental_service.exception.InvalidDiscountException;
+import com.shiva.rental_service.exception.NoChargeableDaysException;
 import com.shiva.rental_service.services.CheckoutService;
 import com.shiva.rental_service.services.HolidayService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -13,17 +17,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutServiceTest {
+    private static Map<String, Tool> toolInventory;
+    private static CheckoutService checkoutService;
 
-    @Test
-    public void testCheckout() {
-        Map<String, Tool> toolInventory = new HashMap<>();
+    @BeforeAll
+    public static void setup() {
+        toolInventory = new HashMap<>();
         toolInventory.put("CHNS", new Tool("CHNS", "Chainsaw", "Stihl", 1.49, false, true));
         toolInventory.put("LADW", new Tool("LADW", "Ladder", "Werner", 1.99, true, false));
         toolInventory.put("JAKD", new Tool("JAKD", "Jackhammer", "DeWalt", 2.99, false, false));
         toolInventory.put("JAKR", new Tool("JAKR", "Jackhammer", "Ridgid", 2.99, false, false));
 
         HolidayService holidayService = new HolidayService();
-        CheckoutService checkoutService = new CheckoutService(toolInventory, holidayService);
+        checkoutService = new CheckoutService(toolInventory, holidayService);
+    }
+
+
+    @Test
+    public void testCheckout() {
 
         // Add test cases
         Map<Tool, Integer> toolQuantities1 = new HashMap<>();
@@ -40,5 +51,35 @@ public class CheckoutServiceTest {
         Assertions.assertEquals(agreement2.getToolCode(), "CHNS");
         Assertions.assertEquals(agreement2.getFinalCharge(), 4.47, 0.01);
 
+    }
+
+    @Test
+    public void testInvalidDiscountException() {
+        // Test Case: Discount is more than 100%
+        Map<Tool, Integer> toolQuantities = new HashMap<>();
+        toolQuantities.put(toolInventory.get("LADW"), 1);
+        Cart cart = new Cart(toolQuantities, LocalDate.of(2024, 6, 23), LocalDate.of(2024, 6, 27));
+
+        Assertions.assertThrows(InvalidDiscountException.class, () -> checkoutService.checkout(cart, 101));
+    }
+
+    @Test
+    public void testInvalidDateRangeException() {
+        // Test Case: To-date is less than from-date
+        Map<Tool, Integer> toolQuantities = new HashMap<>();
+        toolQuantities.put(toolInventory.get("CHNS"), 1);
+        Cart cart = new Cart(toolQuantities, LocalDate.of(2024, 6, 27), LocalDate.of(2024, 6, 23));
+
+        Assertions.assertThrows(InvalidDateRangeException.class, () -> checkoutService.checkout(cart, 10));
+    }
+
+    @Test
+    public void testNoChargeableDaysException() {
+        // Test Case: No chargeable days after excluding holidays and weekends
+        Map<Tool, Integer> toolQuantities = new HashMap<>();
+        toolQuantities.put(toolInventory.get("JAKD"), 1);
+        Cart cart = new Cart(toolQuantities, LocalDate.of(2024, 7, 4), LocalDate.of(2024, 7, 4)); // Independence Day
+
+        Assertions.assertThrows(NoChargeableDaysException.class, () -> checkoutService.checkout(cart, 10));
     }
 }
